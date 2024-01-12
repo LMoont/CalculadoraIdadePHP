18a3a83b902f3de0b74788a4d3f0089906b4ec38
Lucas Montino <lucasmontino@hotmail.com>
2024-01-12 18:34:30 -0300

    Alteração do title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Idade</title>
+    <title>Calculadora de Idade</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
